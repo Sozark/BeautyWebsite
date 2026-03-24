0375118845c7cda02eaa1a57c60d1135dfed19d1
Noah Jones <njone55@lsu.edu>
2026-03-24 17:04:52 -0500

    Added Contact Page and Started implementing hamburger slider
    There are bugs regrading the slider that include:
    
    Slider not activating when hamburger logo is pressed
    
    Cart Icon is misalighned

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,12 +1,21 @@
-const hamburger = document.getElementById("hamburger"); 
+const hamburger = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("mobileMenu");
+const overlay = document.getElementById("overlay");
 
 hamburger.addEventListener("click", () => {
-    mobileMenu.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+  overlay.classList.remove("active");
 });
 
 document.addEventListener("click", (e) => {
   if (!mobileMenu.contains(e.target) && !hamburger.contains(e.target)) {
     mobileMenu.classList.remove("active");
+    overlay.classList.remove("active");
   }
 });
+
